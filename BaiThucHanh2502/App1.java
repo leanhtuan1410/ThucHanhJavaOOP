5f0dd29266c427422643eba2ebc8e6e8c7c6df34
leanhtuan1410 <97715091+leanhtuan1410@users.noreply.github.com>
2022-02-26 18:15:08 +0700

    Add files via upload

---
+++ BaiThucHanh2502/App1.java
@@ -0,0 +1,23 @@
+package javaapplication15;
+import java.util.Scanner;
+
+public class App1 {
+    public static Void main (String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap so nguyen duong a: ");
+        a = scanner.nextInt();
+        System.out.println("Nhap so nguyen duong b: ");
+        b = scanner.nextInt();
+
+        while (a = b){
+            if ( a > b)
+                a = a-b;
+            else 
+                b = b-a;
+        }
+        System.out.println("Uoc chung lon nhat la :%d, b");
+    }
+        
+    }
+
---
+++ BaiThucHanh2502/App2.java
@@ -0,0 +1,3 @@
+import java.util.Sccanner{
+    public class App2 {
+        
